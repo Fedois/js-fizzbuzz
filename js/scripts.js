e0af7494c0c1d338ea8db11668cb2ddf49642467
Fedois <fedefox14@gmail.com>
2022-12-21 17:38:51 +0100

    stampato tutto in html

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,35 @@
 let containerSmall = document.getElementById('container-small');
+let content
 
 for (square = 1; square <= 20; square++) {
 
-    containerSmall.innerHTML += '<div class="box">' + (square) + '</div>';
 
     if(square % 3 == 0 && square % 5 == 0){
         console.log(square, 'fizzbuzz');
 
+        content = 'fizzbuzz'
 
         
-    
     }
     else if(square % 3 == 0){
-        console.log(square, 'fizz')
+        console.log(square, 'fizz');
 
-   
+        content = 'fizz'
 
     }
     else if(square % 5 == 0){
-        console.log(square, 'buzz')
+        console.log(square, 'buzz');
+
+        content = 'buzz'
 
     }
     else{
-        console.log(square)
+        console.log(square);
 
-
+        content = square;
 
     }
+
+containerSmall.innerHTML += '<div class="box">' + (content) + '</div>';
+
 }
