636a63acc30cea2792f1754b55a7b6137cbed30b
Fedois <fedefox14@gmail.com>
2022-12-21 16:45:44 +0100

    fine console e aggiunta square

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,30 @@
+let containerSmall = document.getElementById('container-small');
 
- for (square = 1; square <= 20; square++) {
+for (square = 1; square <= 20; square++) {
+
+    containerSmall.innerHTML += '<div class="box">' + (square) + '</div>';
 
     if(square % 3 == 0 && square % 5 == 0){
         console.log(square, 'fizzbuzz');
+
+
+        
     
     }
     else if(square % 3 == 0){
         console.log(square, 'fizz')
+
+   
+
     }
     else if(square % 5 == 0){
         console.log(square, 'buzz')
+
     }
     else{
         console.log(square)
+
+
+
     }
 }
